1877e4dcaba01a9edd5856a02355aa89595eafe5
Cheefs <cheefst@outlook.com>
2022-06-19 18:34:30 +0700

    [+] реализация апи для управления сотрудниками

--- WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -1,58 +1,57 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
 
-namespace WebStore.WebAPI.Clients.Base
+namespace WebStore.WebAPI.Clients.Base;
+
+public abstract class BaseClient
 {
-    public abstract class BaseClient
+    protected HttpClient Http { get; }
+    protected string Address { get; }
+    protected BaseClient(HttpClient client, string address)
     {
-        protected HttpClient Http { get; }
-        protected string Address { get; }
-        protected BaseClient(HttpClient client, string address)
+        Http = client;
+        Address = address;
+    }
+
+    protected T? Get<T>(string url) => GetAsync<T>(url).Result;
+
+    protected async Task<T?> GetAsync<T>(string url, CancellationToken cancellationToken = default)
+    {
+        var response = await Http.GetAsync(url, cancellationToken).ConfigureAwait(false);
+
+        switch (response.StatusCode)
         {
-            Http = client;
-            Address = address;
+            case HttpStatusCode.NoContent:
+            case HttpStatusCode.NotFound:
+                return default;
+            default:
+                return await response
+                    .EnsureSuccessStatusCode()
+                    .Content
+                    .ReadFromJsonAsync<T>();
         }
+    }
 
-        protected T? Get<T>(string url) => GetAsync<T>(url).Result;
+    protected HttpResponseMessage Post<T>(string url, T body) => PostAsync(url, body).Result;
 
-        protected async Task<T?> GetAsync<T>(string url, CancellationToken cancellationToken = default)
-        {
-            var response = await Http.GetAsync(url, cancellationToken).ConfigureAwait(false);
+    protected async Task<HttpResponseMessage> PostAsync<T>(string url, T body, CancellationToken cancellationToken = default)
+    {
+        var response = await Http.PostAsJsonAsync(url, body, cancellationToken).ConfigureAwait(false);
+        return response.EnsureSuccessStatusCode();
+    }
 
-            switch (response.StatusCode)
-            {
-                case HttpStatusCode.NoContent:
-                case HttpStatusCode.NotFound:
-                    return default;
-                default:
-                    return await response
-                        .EnsureSuccessStatusCode()
-                        .Content
-                        .ReadFromJsonAsync<T>();
-            }
-        }
+    protected HttpResponseMessage Put<T>(string url, T body) => PutAsync(url, body).Result;
 
-        protected HttpResponseMessage Post<T>(string url, T body) => PostAsync(url, body).Result;
+    protected async Task<HttpResponseMessage> PutAsync<T>(string url, T body, CancellationToken cancellationToken = default)
+    {
+        var response = await Http.PostAsJsonAsync(url, body, cancellationToken).ConfigureAwait(false);
+        return response.EnsureSuccessStatusCode();
+    }
 
-        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T body, CancellationToken cancellationToken = default)
-        {
-            var response = await Http.PostAsJsonAsync(url, body, cancellationToken).ConfigureAwait(false);
-            return response.EnsureSuccessStatusCode();
-        }
+    protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
 
-        protected HttpResponseMessage Put<T>(string url, T body) => PutAsync(url, body).Result;
-
-        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T body, CancellationToken cancellationToken = default)
-        {
-            var response = await Http.PostAsJsonAsync(url, body, cancellationToken).ConfigureAwait(false);
-            return response.EnsureSuccessStatusCode();
-        }
-
-        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
-
-        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken cancellationToken = default)
-        {
-            return await Http.DeleteAsync(url, cancellationToken).ConfigureAwait(false);
-        }
+    protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken cancellationToken = default)
+    {
+        return await Http.DeleteAsync(url, cancellationToken).ConfigureAwait(false);
     }
 }
---
+++ WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(HttpClient client) : base(client, "api/employees") { }
+
+        public int GetCount() => Get<int>($"{Address}/count");
+
+        public IEnumerable<Employee> Get(int skip, int take) => Get<IEnumerable<Employee>>($"{Address}/{skip}/{take}") ?? Enumerable.Empty<Employee>();
+
+        public Employee? GetById(int id) => Get<Employee>($"{Address}/{id}");
+
+        public IEnumerable<Employee> GetAll() => Get<IEnumerable<Employee>>(Address) ?? Enumerable.Empty<Employee>();
+
+        public int Add(Employee employee)
+        {
+            var response = Post(Address, employee);
+            var newEmploye = response.Content.ReadFromJsonAsync<Employee>().Result;
+
+            if(newEmploye is null)
+            {
+                throw new InvalidOperationException("Не удалось добавить сотрудника");
+            }
+            var id = newEmploye.Id;
+            employee.Id = id;
+
+            return id;
+        }
+
+        public bool Edit(Employee employee)
+        {
+            var response = Put(Address, employee);
+            return response.StatusCode == HttpStatusCode.OK; 
+        }
+
+        public bool Delete(int id)
+        {
+            var response = Delete($"{Address}/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
