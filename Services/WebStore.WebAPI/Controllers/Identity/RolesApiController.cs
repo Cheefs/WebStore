9847a187b7073c4a4ce6085e1075ec5b62dc986c
Cheefs <cheefst@outlook.com>
2022-06-22 21:22:53 +0700

    [+] Добавление интерфейсов Identity и заготовок для контроллеров roles и users

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.WebAPI.Controllers.Identity;
+
+[ApiController]
+[Route("api/roles")]
+public class RolesApiController : ControllerBase
+{
+    private readonly RoleStore<Role> _rolesStore;
+    private readonly ILogger<RolesApiController> _logger;
+
+    public RolesApiController(WebStoreDB webStoreDb, ILogger<RolesApiController> logger)
+    {
+        _logger = logger;
+        _rolesStore = new(webStoreDb);
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Role>> GetAll() => await _rolesStore.Roles.ToArrayAsync();
+}
---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.WebAPI.Controllers.Identity;
+
+[ApiController]
+[Route("api/users")]
+public class UsersApiController : ControllerBase
+{
+    private readonly ILogger<UsersApiController> _logger;
+    private readonly UserStore<User,Role, WebStoreDB> _userStore;
+
+    public UsersApiController(WebStoreDB webStoreDB, ILogger<UsersApiController> logger)
+    {
+        _logger = logger;
+        _userStore = new (webStoreDB);
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<User>> GetAll() => await _userStore.Users.ToArrayAsync();
+}
+
