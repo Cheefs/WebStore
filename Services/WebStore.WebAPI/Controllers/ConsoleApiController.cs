3f99d08f7353da4bd4f52b6dd23f9682f5879674
Cheefs <cheefst@outlook.com>
2022-06-26 15:14:59 +0700

    [+] Конфигурация системы логирования log4net

---
+++ Services/WebStore.WebAPI/Controllers/ConsoleApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route(WebApiAdresses.V1.Console)]
+public class ConsoleApiController : ControllerBase
+{
+    [HttpGet("clear")]
+    public void Clear() => Console.Clear();
+
+    [HttpGet("write/{str}")]
+    public void Write(string str) => Console.WriteLine(str);
+}
